affe4d9b346d4318719da3906d870361e0a1436c
Abdul Wahid <abdulwahidkhuhro1@gmail.com>
2022-01-27 22:23:28 +0500

    tried to add puzzle solving logic

--- PuzzleGame.java
+++ PuzzleGame.java
@@ -1,5 +1,6 @@
 import java.awt.*;
-public class PuzzleGame {
+import java.awt.event.*;
+public class PuzzleGame implements ActionListener{
 	Frame window = new Frame();
 	Button[] button = new Button[16];
 	Font font = new Font("SansSerif", Font.BOLD, 45);
@@ -11,10 +12,11 @@ public class PuzzleGame {
 		
 		for(int i = 0; i < button.length; i++){
 			if( i == 0)
-				button[i] = new Button();
+				button[i] = new Button("");
 			else
 				button[i] = new Button(i+"");
 			button[i].setFont(font);
+			button[i].addActionListener(this);
 			window.add(button[i]);
 		}
 		
@@ -41,6 +43,95 @@ public class PuzzleGame {
 		
 		window.setTitle(window.getWidth()+"");
 		window.show();
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		if(button[0] == e.getSource()){
+			moveBox(Integer.parseInt(button[0].getLabel()));
+		}else if(button[1] == e.getSource()){
+			moveBox(Integer.parseInt(button[1].getLabel()));
+		}else if(button[2] == e.getSource()){
+			moveBox(Integer.parseInt(button[2].getLabel()));
+		}else if(button[3] == e.getSource()){
+			moveBox(Integer.parseInt(button[3].getLabel()));
+		}else if(button[4] == e.getSource()){
+			moveBox(Integer.parseInt(button[4].getLabel()));
+		}else if(button[5] == e.getSource()){
+			moveBox(Integer.parseInt(button[5].getLabel()));
+		}else if(button[6] == e.getSource()){
+			
+		}else if(button[7] == e.getSource()){
+			
+		}else if(button[8] == e.getSource()){
+			
+		}else if(button[9] == e.getSource()){
+			
+		}else if(button[10] == e.getSource()){
+			
+		}else if(button[11] == e.getSource()){
+			
+		}else if(button[12] == e.getSource()){
+			
+		}else if(button[13] == e.getSource()){
+			
+		}else if(button[14] == e.getSource()){
+			
+		}else if(button[15] == e.getSource()){
+			
+		}
+	}
+	
+	private void moveBox(int selected){
+		int left = selected - 1,up = selected - 4,right = selected + 1,down = selected + 4;
+		if(up < 0){
+			/*if(left < 0){
+					if(indexOfEmptyBox != -2){
+						int destination = indexOfEmptyBox(left, up, right, down);
+						button[destination].setLabel(selected+"");
+						button[selected].setLabel("");
+					}
+				}*/
+			int destination = indexOfEmptyBox(left, up, right, down);
+			if(destination != -2){
+				button[destination].setLabel(selected+"");
+				button[selected].setLabel("");
+			}
+		}else if(down > 15){
+			int destination = indexOfEmptyBox(left, up, right, down);
+			if(destination != -2){
+				button[destination].setLabel(selected+"");
+				button[selected].setLabel("");
+			}
+		}else{
+			int destination = indexOfEmptyBox(left, up, right, down);
+			if(destination != -2){
+				button[destination].setLabel(selected+"");
+				button[selected].setLabel("");
+			}
+		}
+		
+	}
+	
+	private void ifPossibleMoveIs(){
+		
+	}
+	private int indexOfEmptyBox(int left, int up, int right, int down){
+		if(isEmpty(left)){
+			return left;
+		}else if(isEmpty(up)){
+			return up;
+		}else if(isEmpty(right)){
+			return right;
+		}else if(isEmpty(down)){
+			return down;
+		}
+		return -2;
+	}
+	
+	private boolean isEmpty(int choice){
+		if(choice < 0)
+			return false;
+		return button[choice].getLabel().equals("");
 	}
 	public static void main(String[] args){
 		new PuzzleGame();
